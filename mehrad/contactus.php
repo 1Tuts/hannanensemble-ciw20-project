cbf7c0d18a2eb0f6b6c423b2daf861101f598bed
mkdesign <me@mkdesign.ir>
2012-10-15 23:20:14 +0330

    error in push

--- mehrad/contactus.php
+++ mehrad/contactus.php
@@ -48,13 +48,7 @@ body{
 		<div class="title"></div>
 		<div class="contact-ways">
 			<div class="send-message">
-				<h4>ارسال پیام</h4>
-				<form action="mail.php" id="contact" method="post">
-					<input type="text" class="text" name="name" id="name" placeholder="نام" />
-					<input type="text" class="text" name="email" id="email" placeholder="ایمیل" />
-					<textarea name="message" id="message" cols="65" rows="16" placeholder="پیام"></textarea>
-					<input type="submit" value="ارسال" class="button" id="submit" name="submit" />
-				</form>
+				
 			</div>
 			<div class="contact-info">
 				<h4>اطلاعات تماس</h4>
