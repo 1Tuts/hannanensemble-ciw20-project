8a70b77b896c4c33d39d35af888d7dab675d0909
mkdesign <me@mkdesign.ir>
2012-10-16 11:58:12 +0330

    start to work on email form validate

--- mehrad/scripts.js
+++ mehrad/scripts.js
@@ -1,3 +1,13 @@
+function validateText(str,len){
+	return str.length >= len;
+}
+
+function validateEmail(str){
+	var emailPattern = /^[a-z0-9+_%.-]+@(?:[a-z0-9-]+\.)+[a-z]{2,6}$/i ;
+
+	return emailPattern.test(str);
+}
+
 $(function(){
 //-------------------------------- action menu ----------------
 window.onscroll=function(){
