498cef5e69756e21335e7e66a0af049d6c8ae3fb
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-16 19:32:32 +0330

    copy to root and change exe to php

--- index.php
+++ index.php
@@ -1,66 +1,58 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-    <meta charset="UTF-8">
-    <title>Hannan ensemble</title>
+    <meta charset="utf-8" />
+    <title>Untitled Document</title>
     <link type="text/css" rel="stylesheet" href="reset.css" />
     <link type="text/css" rel="stylesheet" href="1styles.css" />
     <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 
 <body>
-<!--/////////// Header Part /////////////-->
 	<header class="head">
     	<div class="grayline"></div>	
-        <section class="header container">
-        	<nav class="menubar w10 right last"></nav>
-            <div class="logo w4"></div>
+        <section class="header">
+            <nav></nav>
+        	<div class="logo"></div>
             <div class="clear"></div>
-            <div class="hline"></div>
         </section>
-    </header>    
-<!--/////////// Middle Part /////////////-->
-    <section class="middle">
-    	<section class="container">
-        	<div class="title"></div>
+    </header>
+    <section class="contain">
+        <div class="middle">
+            <div class="title"></div>
             <div class="banner"></div>
             <div class="boxess">
-            	<div class="first w6 left"></div>
-                <div class="second w6 left"></div>
-                <div class="third w6 left"></div>
-                <div class="forth ml18 last"></div>
+                <div class="first" id="boxone"></div>
+                <div class="second" id="boxone"></div>
+                <div class="third" id="boxone"></div>
+                <div class="forth"></div>
                 <div class="clear"></div>
             </div>
-            
-            <!-- RECENT WORK -->
             <div class="recentwork">
-            	<div class="first w6 left"></div>
-                <div class="second w6 left"></div>
-                <div class="third w6 left"></div>
-                <div class="forth ml18 last"></div>
+                <div class="first" id="boxtwo"></div>
+                <div class="second" id="boxtwo"></div>
+                <div class="third" id="boxtwo"></div>
+                <div class="forth"></div>
                 <div class="clear"></div>
             </div>
-        </section>
+        </div>
     </section>
-<!--/////////// Footer Part /////////////-->
-    <footer class="foot">
-    	<section class="contain">
-        	<div class="bigfooter">
-            	<div class="first w6 left"></div>
-                <div class="second w6 left"></div>
-                <div class="third w6 left"></div>
-                <div class="forth ml18 last"></div>
-                <div class="clear"></div>
-            </div>
-            
-            <!-- Copyright Part -->
-            <div class="footer">
-            	<div class="copyright left w8"></div>
-                <div class="facebook ml15 last"></div>
-                <div class="clear"></div>
-            </div>
+    <footer class="contain">
+        <section class="foot">
+                <div class="bigfooter">
+                    <div class="first" id="boxthree"></div>
+                    <div class="second" id="boxthree"></div>
+                    <div class="third" id="boxthree"></div>
+                    <div class="forth"></div>
+                    <div class="clear"></div>
+                </div>
+                <div class="footer">
+                    <div class="copyright"></div>
+                    <div class="facebook"></div>
+                    <div class="clear"></div>
+                </div>           
         </section>
-        <div class="darkline"></div>
     </footer>
+    <div class="darkline"></div>
 </body>
 </html>
