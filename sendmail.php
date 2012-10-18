a7008a68ce97c18d15e16645c2a9007ab52917b9
mkdesign <me@mkdesign.ir>
2012-10-18 21:29:39 +0330

    start to work on sendmail.php

---
+++ sendmail.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>Sending Mail ...</title>
+	<style type="text/css">
+
+		.ok{
+			color:#23B3AB;
+		}
+
+		.err {
+			color:#BD5B3D;
+		}
+
+	</style>
+</head>
+<body>
