6daec75ac19b21bd0f9aaefc9e95f1ea8010d259
mkdesign <me@mkdesign.ir>
2012-10-18 21:30:05 +0330

    adding variables

--- sendmail.php
+++ sendmail.php
@@ -16,3 +16,13 @@
 	</style>
 </head>
 <body>
+
+	<?php
+
+	error_reporting(E_ALL ^ E_NOTICE);
+
+	$admin   = 'info@zahmati.com';
+
+	$name    = $_POST['name'];
+	$mail    = $_POST['email'];
+	$message = $_POST['message'];
