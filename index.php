55a0dd617fed22945f7cef67ca1b9cfbf66794be
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-20 00:06:38 +0330

    fix err

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 </head>
 
 <body>
+	<!-- ---------------- Header ------------------- -->
 	<header class="normal">
 		<div class="grayline"></div>	
 		<section class="header">
@@ -49,6 +50,7 @@
 			<div class="clear"></div>
 		</section>
 	</header>
+	<!-- ---------------- Middle ------------------- -->
 	<section class="contain">
 		<div class="middle">
 			<div class="title"></div>
@@ -81,14 +83,43 @@
 				<div class="clear"></div>
 			</div>
 			<div class="recentwork">
-				<div class="first" id="boxtwo"></div>
-				<div class="second" id="boxtwo"></div>
-				<div class="third" id="boxtwo"></div>
-				<div class="forth"></div>
+				<div class="first">
+					<img src="images/tanboor.jpg" />
+					<p>درباره ی تنبـــور</p>
+					<div class="back">
+						<h4>درباره ی تنبـــور</h4>
+						<h6>در این قسمت به گردآوری مقالات و نظرات اساتید و پیشکسوتان و هر آنچه در مورد تنبــور گفته شده میپردازیم .</h6>	
+					</div>
+				</div>
+				<div class="second">
+					<img src="images/album.jpg" />
+					<p>آلبوم های تنبـــور</p>
+					<div class="back">
+						<h4>آلبوم های تنبـــور</h4>
+						<h6>هدف این قسمت یادآوری آثار ماندگار تنبــور و معرفی آلبوم های جدید تنبــور میباشد .</h6>
+					</div>
+				</div>
+				<div class="third">
+					<img src="images/books.jpg" />
+					<p>کتابهای تنبــــور</p>
+					<div class="back">
+						<h4>کتابهای تنبــــور</h4>
+						<h6>در این قسمت کتب و جزواتی را که درباره تنبور به چاپ رسیده باشد را به نمایش میگزاریم .</h6>
+					</div>
+				</div>
+				<div class="forth">
+					<img src="images/bozorgan.jpg" />
+					<p>بزرگان تنبـــور</p>
+					<div class="back">
+						<h4>بزرگان تنبـــور</h4>
+						<h6>هنردوستــان عزیز در این قسمت شما میتوانید با نوازندگان نامدار تنبـــور آشنا شوید و سرگذشت<br /> و آثار آنان را مطالعه نمائید .</h6>
+					</div>
+				</div>
 				<div class="clear"></div>
 			</div>
 		</div>
 	</section>
+	<!-- ---------------- Footer ------------------- -->
 	<footer class="foot">
 		<section class="contain">
 			<div class="bigfooter">
@@ -98,20 +129,26 @@
 					   <p>لینکهای ارتباطی تنبورنوازان در شبکه های اجتماعی</p>
 					   <br /><br />
 					   <ul>
-						   <li class="fb" id="trans"><a target="_blank"></a></li>
-						   <li class="twitter" id="trans"><a target="_blank"></a></li>
-						   <li class="flickr" id="trans"><a target="_blank"></a></li>
-						   <li class="rss" id="trans"><a target="_blank"></a></li>
+						   <li class="fb" id="trans"><a href="http://www.facebook.com/TanboornavazaneHanan" target="_blank"></a></li>
+						   <li class="twitter" id="trans"><a href="https://twitter.com/HananEnsemble" target="_blank"></a></li>
+						   <li class="flickr" id="trans"><a href="http://www.flickr.com/photos/hananensemble/" target="_blank"></a></li>
+						   <li class="rss" id="trans"><a href="" target="_blank"></a></li>
 					   </ul>
 				</div>
 				<div class="second">
 					<h3>آخرین پست ها</h3>
+					<p><a>دیدار با استاد طاهر یارویسی</a></p>
 				</div>
 				<div class="third">
 					<h3>آخرین اخبار</h3>
+					<p><a>آلبوم جدید حسام الدین سراج</a></p>
+					<p><a>کنسرت بزرگ از غرب تا شرق در مشهد</a></p>
 				</div>
 				<div class="forth">
 					<h3>عضویت در خبرنامه</h3>
+					<p>لطفا ایمیل خود را وارد کنید</p>
+					<input type="input">
+					<input class="button" type="submit" value="ارسال">
 				</div>
 				<div class="clear"></div>
 			</div>
@@ -124,10 +161,8 @@
 				</div>
 				<div class="clear"></div>
 			</div>
-			
 		</section>
 		<div class="darkline"></div>
 	</footer>
-	<div class="darkline"></div>
 </body>
 </html>
