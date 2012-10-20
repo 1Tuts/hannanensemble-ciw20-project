2287a737285851e9a1b0ddd5824e9dfcfe3bdc3c
etp1711 <etpetp1711@gmail.com>
2012-10-20 09:57:02 +0330

    fix eror

---
+++ Etp1711/sendmail.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8" />
+	<title>Sending Mail ...</title>
+	<style type="text/css">
+
+		.ok{
+			color:#23B3AB;
+		}
+
+		.err {
+			color:#BD5B3D;
+		}
+
+	</style>
+</head>
+<body>
+
+	<?php
+
+	error_reporting(E_ALL ^ E_NOTICE);
+
+	$admin   = 'yourmail@gmail.com';
+
+	$name    = $_POST['name'];
+	$mail    = $_POST['email'];
+	$message = $_POST['message'];
+	if( strlen($name) >= 3 && strlen($mail) >= 7 && strlen($message) >= 10){
+		if( @mail(
+			$admin,
+
+			" zahmati.com contact : $name <$email>",
+
+			$message)
+			){
+
+			echo "<h1 class='ok'>Your Mail Sent!</h1>";
+
+		}else {
+
+			echo "<h1 class='err'>Error In Sending Mail</h1>";
+		}
+		
+	}else{
+
+		echo "<h1 class='err'>Access Restricted!!!</h1>";
+	}
+	?>
+</body>
+</html>
