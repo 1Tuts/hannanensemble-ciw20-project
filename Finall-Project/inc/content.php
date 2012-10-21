1ff5eb7a7df90c5ecbbfe7ee5f108b3ad5ecc844
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-21 18:50:29 +0330

    Unencrypted project

---
+++ Finall-Project/inc/content.php
@@ -0,0 +1,84 @@
+	<section class="contain">
+		<div class="middle">
+			<div class="title"></div>
+			<div class="banner">
+				<?php
+
+					$images_path = './images/slide';
+					$images      = scandir($images_path);
+
+					foreach ($images as $img) {
+
+						$img_exp = explode('.' , $img);
+
+						$img_exp_end = end($img_exp );
+
+						$img_type = strtolower($img_exp_end );
+
+						if($img_type == 'jpg' || $img_type == 'png'){
+
+							echo "<div style=\"background-image : url('$images_path/$img');\"></div>";
+						}
+					}
+				?>         
+			</div>
+			<div class="boxess">
+				<div class="first">
+					<h3>نوازندگــان</h3>
+					<img src="images/navazande.png">
+					<p>معرفی نوازنده گان تنبور نوازان حنان</p>
+				</div>
+				<div class="second">
+					<h3>اخبــار</h3>
+					<img src="images/akhbar.png">
+					<p>اخبار کنسرتها و جشنواره های موسیقی</p>
+				</div>
+				<div class="third">
+					<h3>نمایه هــا</h3>
+					<img src="images/navazande.png">
+					<p>نمایش نمایه های حنـــان</p>
+				</div>
+				<div class="forth">
+					<h3>برنامه ها</h3>
+					<img src="images/navazande.png">
+					<p>نوازهای اجرا شده ی حنـــان</p>
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="recentwork">
+				<div class="first">
+					<img src="images/tanboor.jpg" />
+					<p>درباره ی تنبـــور</p>
+					<div class="back">
+						<h4>درباره ی تنبـــور</h4>
+						<h6>در این قسمت به گردآوری مقالات و نظرات اساتید و پیشکسوتان و هر آنچه در مورد تنبــور گفته شده میپردازیم .</h6>	
+					</div>
+				</div>
+				<div class="second">
+					<img src="images/album.jpg" />
+					<p>آلبوم های تنبـــور</p>
+					<div class="back">
+						<h4>آلبوم های تنبـــور</h4>
+						<h6>هدف این قسمت یادآوری آثار ماندگار تنبــور و معرفی آلبوم های جدید تنبــور میباشد .</h6>
+					</div>
+				</div>
+				<div class="third">
+					<img src="images/books.jpg" />
+					<p>کتابهای تنبــــور</p>
+					<div class="back">
+						<h4>کتابهای تنبــــور</h4>
+						<h6>در این قسمت کتب و جزواتی را که درباره تنبور به چاپ رسیده باشد را به نمایش میگزاریم .</h6>
+					</div>
+				</div>
+				<div class="forth">
+					<img src="images/bozorgan.jpg" />
+					<p>بزرگان تنبـــور</p>
+					<div class="back">
+						<h4>بزرگان تنبـــور</h4>
+						<h6>هنردوستــان عزیز در این قسمت شما میتوانید با نوازندگان نامدار تنبـــور آشنا شوید و سرگذشت<br /> و آثار آنان را مطالعه نمائید .</h6>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	</section>
---
+++ Finall-Project/inc/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8" />
+	<title>Untitled Document</title>
+	<link type="text/css" rel="stylesheet" href="reset.css" />
+	<link type="text/css" rel="stylesheet" href="1styles.css" />
+	<link type="text/css" rel="stylesheet" href="style.css" />
+	<script type="text/javascript" src="js/zepto.js"></script>
+	<script type="text/javascript" src="js/scripts.js"></script>
+	<script type="text/javascript" src="js/slide.js"></script>
+	<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<header class="normal">
+		<div class="grayline"></div>	
+		<section class="header">
+			<nav>
+				<menu>
+					<li><a href="./">صفحه اصلی<span>|</span></a></li>
+					<li><a href="./aboutus.php">درباره گروه<span>|</span></a></li>
+					<li><a href="#">اعضا<span>|</span></a></li>
+					<li><a href="#">اخبار<span>|</span></a></li>
+					<li><a href="./contactus.php">تماس با ما<span></span></a></li>
+					<li class="clear"></li>
+				</menu>
+			</nav>
+			<div class="logo"></div>
+			<div class="clear"></div>
+		</section>
+	</header>
+	<header class="action">
+		<div class="grayline"></div>	
+		<section class="header">
+			<nav>
+				<menu>
+					<li><a href="./">صفحه اصلی<span>|</span></a></li>
+					<li><a href="./aboutus.php">درباره گروه<span>|</span></a></li>
+					<li><a href="#">اعضا<span>|</span></a></li>
+					<li><a href="#">اخبار<span>|</span></a></li>
+					<li><a href="./contactus.php">تماس با ما<span></span></a></li>
+					<li class="clear"></li>
+				</menu>
+			</nav>
+			<div class="logo"></div>
+			<div class="clear"></div>
+		</section>
+	</header>
