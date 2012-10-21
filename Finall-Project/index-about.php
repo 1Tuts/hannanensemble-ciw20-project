401c3b9b2eba350e965bfa098ec2334d64740e32
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-22 00:41:13 +0330

    create aboutus page in wp with content

---
+++ Finall-Project/index-about.php
@@ -0,0 +1,12 @@
+<?php
+
+    /*
+        Template Name: About
+    */
+
+    get_template_part('./inc/header');
+
+    get_template_part('./inc/about','content');
+
+    get_template_part('./inc/footer');
+?>
