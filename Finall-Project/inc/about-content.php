869aade346c21ac20df50a542d16c3f8c7b1ab74
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-22 01:34:20 +0330

    add image post in wp

--- Finall-Project/inc/about-content.php
+++ Finall-Project/inc/about-content.php
@@ -16,8 +16,20 @@
                             <p><?php the_content(); ?></p>  
                         </div>  
 
-                        <div class="pic"></div>
-                        
+                        <div class="pic">
+                            <?php
+                                if (count($meta['img']>0)){
+                                    echo "<div class='about-img'>";
+                                    foreach ($meta['img'] as $img_id) {
+                                        $img_small= wp_get_attachment_image('$img_id','thumbnail');
+                                        $img_larg= wp_get_attachment_image_src('$img_id','large');
+                                        echo "<img src='$img_larg[0]' width='250' />";
+                                    }
+                                    echo "</div>";
+                                }
+                            ?>
+                        </div>
+
                         <div class="clear"></div>
                     </article>
                     
