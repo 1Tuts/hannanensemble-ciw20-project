427d62c3fa4b4518039d763ecae94cb41683e0f8
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-21 20:06:22 +0330

    edit php code of slideshow

--- Finall-Project/inc/content.php
+++ Finall-Project/inc/content.php
@@ -3,18 +3,14 @@
 			<div class="title"></div>
 			<div class="banner">
 				<?php
-
-					$images_path = './images/slide';
+					$images_path = "images/slide";
 					$images      = scandir($images_path);
-
+					
 					foreach ($images as $img) {
-
 						$img_exp = explode('.' , $img);
-
-						$img_exp_end = end($img_exp );
-
-						$img_type = strtolower($img_exp_end );
-
+						$img_exp_end = end($img_exp);
+						$img_type = strtolower($img_exp_end);
+						
 						if($img_type == 'jpg' || $img_type == 'png'){
 
 							echo "<div style=\"background-image : url('$images_path/$img');\"></div>";
