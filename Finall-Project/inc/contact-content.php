4ba0c87eda316a4811f43b03f1c9d6a93b755b08
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-22 10:42:01 +0330

    create contact us page in wp with post

---
+++ Finall-Project/inc/contact-content.php
@@ -0,0 +1,42 @@
+<section class="contain">
+		<div class="middle">
+
+			<?php
+				if (have_posts()){
+					the_post();
+					$meta= get_post_custom();
+			?>
+
+				<div class="title">
+					<h2><?php the_title(); ?><h2>
+				</div>
+
+				<div class="contact-ways">
+					<div class="send-message">
+						<h4>ارسال پیام</h4>
+						<form action="sendmail.php" id="contact" method="post">
+							<input type="text" class="text" name="name" id="name" placeholder="نام" />
+							<input type="text" class="text" name="email" id="email" placeholder="ایمیل" />
+							<textarea name="message" id="message" cols="65" rows="16" placeholder="پیام"></textarea>
+							<input type="submit" value="ارسال" class="button" id="submit" name="submit" />
+						</form>
+					</div>
+
+					<div class="contact-info">
+						<h4>اطلاعات تماس</h4>
+						<div class="text">
+							<p>
+								<?php the_content(); ?>
+							</p>
+						</div>
+					</div>
+
+					<div class="clear"></div>
+				</div>
+
+			<?php
+				}
+			?>
+
+		</div>
+	</section>
---
+++ Finall-Project/index-contact.php
@@ -0,0 +1,13 @@
+<?php
+	
+	/*
+		Template Name: Contact
+	*/
+
+	get_template_part('./inc/header');
+
+	get_template_part('./inc/contact-content');
+
+	get_template_part('./inc/footer');
+
+?>
