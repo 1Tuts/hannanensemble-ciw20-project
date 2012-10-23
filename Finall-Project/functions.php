7fafb040c62a5e7da6bf498fd9c9b3127a11815b
mkdesign <me@mkdesign.ir>
2012-10-23 19:33:32 +0330

    start to work on index widgets

--- Finall-Project/functions.php
+++ Finall-Project/functions.php
@@ -1,2 +1,12 @@
 <?php
 	add_theme_support('menus');
+
+	register_sidebar(array(
+		'name' => 'Bests',
+		'id' => 'best',
+		'description' => 'bests of tanbour',
+		'before_widget' => '<div class="boxess">',
+		'after_widget' => "</div>",
+		'before_title' => '',
+		'after_title' => ""
+	));
