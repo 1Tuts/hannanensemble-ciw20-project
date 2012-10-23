9a8ba400fe0aeb285e37ced544f578d37ce65d47
mkdesign <me@mkdesign.ir>
2012-10-23 23:08:13 +0330

    functions for wdigets updated

--- Finall-Project/functions.php
+++ Finall-Project/functions.php
@@ -4,8 +4,8 @@
 	register_sidebar(array(
 		'name' => 'Recent',
 		'id' => 'best',
-		'description' => 'bests of tanbour',
-		'before_widget' => "<div class='first'>",
+		'description' => 'recent stuffs',
+		'before_widget' => "<div>",
 		'after_widget' => "</div>",	
 		'before_title' => '<h3>',
 		'after_title' => "</h3>"
