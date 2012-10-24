3f1dd833497b3d3bcc1796f845fa7d6fdfb01da1
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-24 19:27:37 +0330

    add light box to gallery page

--- Finall-Project/inc/header.php
+++ Finall-Project/inc/header.php
@@ -4,9 +4,13 @@
 	<meta charset="utf-8" />
 	<title><?php bloginfo('name'); wp_title();  ?></title>
 	<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/1styles.css" />
+	<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/lightbox.css" />
 	<link type="text/css" rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" />
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/zepto.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/jquery-ui.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/scripts.js"></script>
+	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/lightbox.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/slide.js"></script>
 	<!--[if lt IE 9]>
 		<script src="js/html5shiv.js"></script>
@@ -49,3 +53,4 @@
 				<div class="clear"></div>
 			</section>
 		</header>
+	</div>
