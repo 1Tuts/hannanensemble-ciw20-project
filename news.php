c1f4f48c74dcaf4ec4ca53c9c887436dcaafda11
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-25 23:58:30 +0330

    strat to creat news page

---
+++ news.php
@@ -0,0 +1,109 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8" />
+	<title></title>
+	<link type="text/css" rel="stylesheet" href="./1styles.css" />
+	<link type="text/css" rel="stylesheet" href="./lightbox.css" />
+	<link type="text/css" rel="stylesheet" href="./style.css" />
+	<script type="text/javascript" src="./js/zepto.js"></script>
+	<script type="text/javascript" src="./js/jquery.js"></script>
+	<script type="text/javascript" src="./js/jquery-ui.js"></script>
+	<script type="text/javascript" src="./js/scripts.js"></script>
+	<script type="text/javascript" src="./js/lightbox.js"></script>
+	<script type="text/javascript" src="./js/slide.js"></script>
+	<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<div class="grayline"></div>
+	<div class="content-container">
+		<header class="normal">
+			<section class="header">
+				<nav>
+					<menu>
+						<li><a href="./">صفحه اصلی<span>|</span></a></li>
+						<li><a href="./aboutus.php">درباره گروه<span>|</span></a></li>
+						<li><a href="#">اعضا<span>|</span></a></li>
+						<li><a href="#">اخبار<span>|</span></a></li>
+						<li><a href="./contactus.php">تماس با ما<span></span></a></li>
+						<li class="clear"></li>
+					</menu>
+				</nav>
+				<div class="logo"></div>
+				<div class="clear"></div>
+			</section>
+		</header>
+		<header class="action">
+			<div class="grayline"></div>	
+			<section class="header">
+				<nav>
+					<menu>
+						<li><a href="./">صفحه اصلی<span>|</span></a></li>
+						<li><a href="./aboutus.php">درباره گروه<span>|</span></a></li>
+						<li><a href="#">اعضا<span>|</span></a></li>
+						<li><a href="#">اخبار<span>|</span></a></li>
+						<li><a href="./contactus.php">تماس با ما<span></span></a></li>
+						<li class="clear"></li>
+					</menu>
+				</nav>
+				<div class="logo"></div>
+				<div class="clear"></div>
+			</section>
+		</header>
+		<section class="contain">
+			<div class="middle">
+				<div class="title" id='tit'></div>
+				
+			</div>
+		</section>
+		<div class="footer-container">
+	<footer class="foot">
+		<section class="contain">
+			<div class="bigfooter">
+				<div class="first">
+					<h3>درباره ما</h3>
+					   <p><a href="contactus.html">شما هنردوستان میتوانید با ارسال فرم در صفحه تماس با ما با تنبورنوازان در ارتباط باشید.</a></p><br />
+					   <p>لینکهای ارتباطی تنبورنوازان در شبکه های اجتماعی</p>
+					   <br /><br />
+					   <ul>
+						   <li class="fb" id="trans"><a href="http://www.facebook.com/TanboornavazaneHanan" target="_blank"></a></li>
+						   <li class="twitter" id="trans"><a href="https://twitter.com/HananEnsemble" target="_blank"></a></li>
+						   <li class="flickr" id="trans"><a href="http://www.flickr.com/photos/hananensemble/" target="_blank"></a></li>
+						   <li class="rss" id="trans"><a href="" target="_blank"></a></li>
+					   </ul>
+				</div>
+				<div class="second">
+					<h3>آخرین پست ها</h3>
+					<p><a>دیدار با استاد طاهر یارویسی</a></p>
+				</div>
+				<div class="third">
+					<h3>آخرین اخبار</h3>
+					<p><a>آلبوم جدید حسام الدین سراج</a></p>
+					<p><a>کنسرت بزرگ از غرب تا شرق در مشهد</a></p>
+				</div>
+				<div class="forth">
+					<h3>عضویت در خبرنامه</h3>
+					<p>لطفا ایمیل خود را وارد کنید</p>
+					<input type="input">
+					<input class="button" type="submit" value="ارسال">
+				</div>
+				<div class="clear"></div>
+			</div>
+			<div class="footer">
+				<div class="copyright">
+					<p class="rights">Copyright © 2012 By HananEnsemble.</p>
+				</div>
+				<div class="designteam">
+					<p class="Design">Follow Designing Team "DibaDev" On <span><a>Twitter, <a/></span> <span><a>Facebook,</a></span> <span><a href="https://github.com/DibaDev">Github </a></span></p>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</section>
+	</footer>
+	<div class="darkline"></div>
+</div>
+</body>
+</html>
