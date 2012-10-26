30cc0ed0b4ca5df90426b9f2e1ef22539582e40a
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-27 01:48:48 +0330

    create news page in wp

--- Finall-Project/inc/news-content.php
+++ Finall-Project/inc/news-content.php
@@ -1,16 +1,28 @@
 		<section class="contain-news">
 			<div class="middle">
-				<div class="title" id='tit'></div>
-				<div class="news">
-					<div class="pic"></div>
-					<div class="date-text">
-						<div class="date"></div>
-						<div class="title-text">
-							<div class="title"></div>
-							<div class="text"></div>
-							<div class="clear"></div>
+				<div class="title" id='tit'>اخبار</div>
+				
+				<?php
+					while (have_posts()) {
+						the_post();
+					
+				?>
+				
+					<div class="news">
+						<div class="pic"><?php the_post_thumbnail(); ?></div>
+						<div class="date-text">
+							<div class="date"></div>
+							<div class="title-text">
+								<div class="title"><?php the_title(); ?></div>
+								<div class="text"><?php the_content(); ?></div>
+								<div class="clear"></div>
+							</div>
 						</div>
 					</div>
-				</div>
+
+				<?php
+					}
+				?>
+
 			</div>
 		</section>
