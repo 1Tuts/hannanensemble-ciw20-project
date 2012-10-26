1a923ecbbb95dc8d39ae4b2ebe27651469d24657
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-26 12:24:59 +0330

    create style for add to wp

---
+++ Finall-Project/inc/news-content.php
@@ -0,0 +1,16 @@
+		<section class="contain-news">
+			<div class="middle">
+				<div class="title" id='tit'></div>
+				<div class="news">
+					<div class="pic"></div>
+					<div class="date-text">
+						<div class="date"></div>
+						<div class="title-text">
+							<div class="title"></div>
+							<div class="text"></div>
+							<div class="clear"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
