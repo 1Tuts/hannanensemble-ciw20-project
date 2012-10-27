a20491f04c69e4bde30ea5b8314340b2dc08a168
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-27 15:41:36 +0330

    copy file of rot to this folder

--- Finall-Project/inc/news-content.php
+++ Finall-Project/inc/news-content.php
@@ -1,28 +1,21 @@
 		<section class="contain-news">
 			<div class="middle">
-				<div class="title" id='tit'>اخبار</div>
-				
-				<?php
-					while (have_posts()) {
-						the_post();
-					
-				?>
-				
-					<div class="news">
-						<div class="pic"><?php the_post_thumbnail(); ?></div>
-						<div class="date-text">
-							<div class="date"></div>
-							<div class="title-text">
-								<div class="title"><?php the_title(); ?></div>
-								<div class="text"><?php the_content(); ?></div>
-								<div class="clear"></div>
-							</div>
+				<div class="title" id='tit'></div>
+				<div class="news">
+					<div class="date-pic">
+						<div class="date">
+							<div class="month">01</div>
+							<div class="day">18</div>
+							<div class="year">2012</div>
+							<div class="clear"></div>
 						</div>
+						<div class="pic"><img src='./images/slide/slider1.jpg' /></div>
+						<div class="clear"></div>
 					</div>
-
-				<?php
-					}
-				?>
-
+					<div class="title-text">
+						<div class="title"><h2>اخبار داغ داغ</h2></div>
+						<div class="text"><p>کنسرت گروه در روز.. برگزار نمیشود</p></div>
+					</div>	
+				</div>
 			</div>
 		</section>
