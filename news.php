92ac80f3721824c5e5ede008a57739c16c352bf0
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-27 15:33:27 +0330

    change style of news page

--- news.php
+++ news.php
@@ -57,15 +57,20 @@
 			<div class="middle">
 				<div class="title" id='tit'></div>
 				<div class="news">
-					<div class="pic"></div>
-					<div class="date-text">
-						<div class="date"></div>
-						<div class="title-text">
-							<div class="title"></div>
-							<div class="text"></div>
+					<div class="date-pic">
+						<div class="date">
+							<div class="month">01</div>
+							<div class="day">18</div>
+							<div class="year">2012</div>
 							<div class="clear"></div>
 						</div>
+						<div class="pic"><img src='./images/slide/slider1.jpg' /></div>
+						<div class="clear"></div>
 					</div>
+					<div class="title-text">
+						<div class="title"><h2>اخبار داغ داغ</h2></div>
+						<div class="text"><p>کنسرت گروه در روز.. برگزار نمیشود</p></div>
+					</div>	
 				</div>
 			</div>
 		</section>
