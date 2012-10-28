63f688ba7054f3e04a42fda6f1bd7cfb69659e25
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-28 23:47:45 +0330

    del extra files and codes

--- Finall-Project/inc/content.php
+++ Finall-Project/inc/content.php
@@ -19,40 +19,5 @@
 				?>         
 			</div>
 			<?php get_template_part('inc/widget-recent'); ?>
-			<div class="recentwork">
-				<div class="first">
-					<img src="<?php bloginfo('template_url'); ?>/images/tanboor.jpg" />
-					<p>درباره ی تنبـــور</p>
-					<div class="back">
-						<h4>درباره ی تنبـــور</h4>
-						<h6>در این قسمت به گردآوری مقالات و نظرات اساتید و پیشکسوتان و هر آنچه در مورد تنبــور گفته شده میپردازیم .</h6>	
-					</div>
-				</div>
-				<div class="second">
-					<img src="<?php bloginfo('template_url'); ?>/images/album.jpg" />
-					<p>آلبوم های تنبـــور</p>
-					<div class="back">
-						<h4>آلبوم های تنبـــور</h4>
-						<h6>هدف این قسمت یادآوری آثار ماندگار تنبــور و معرفی آلبوم های جدید تنبــور میباشد .</h6>
-					</div>
-				</div>
-				<div class="third">
-					<img src="<?php bloginfo('template_url'); ?>/images/books.jpg" />
-					<p>کتابهای تنبــــور</p>
-					<div class="back">
-						<h4>کتابهای تنبــــور</h4>
-						<h6>در این قسمت کتب و جزواتی را که درباره تنبور به چاپ رسیده باشد را به نمایش میگزاریم .</h6>
-					</div>
-				</div>
-				<div class="forth">
-					<img src="<?php bloginfo('template_url'); ?>/images/bozorgan.jpg" />
-					<p>بزرگان تنبـــور</p>
-					<div class="back">
-						<h4>بزرگان تنبـــور</h4>
-						<h6>هنردوستــان عزیز در این قسمت شما میتوانید با نوازندگان نامدار تنبـــور آشنا شوید و سرگذشت<br /> و آثار آنان را مطالعه نمائید .</h6>
-					</div>
-				</div>
-				<div class="clear"></div>
-			</div>
 		</div>
 	</section>
