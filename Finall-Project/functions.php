102298f4b42dd4a78fb08ee99d65c3a05913cb26
mkdesign <me@mkdesign.ir>
2012-10-28 18:33:08 +0330

    news changed

--- Finall-Project/functions.php
+++ Finall-Project/functions.php
@@ -59,3 +59,41 @@ function news_init(){
 
 	register_post_type('news' , $args);
 }
+//////////////////////////// NEWS TOGHETHER ////////////////////////////////
+
+add_shortcode('news', 'all_news');
+
+function all_news(){
+
+	
+	$products = new WP_Query(array(
+		'post_type' => 'news'
+	));
+
+	$html = '<h3 class="all-news">News</h3>';
+
+	while($products->have_posts()){
+		$products->the_post();
+
+		$title   = get_the_title();
+		$url     = get_permalink();
+		$content = get_the_content();
+
+		$html .= "<div class='news'>
+					<div class='date-pic'>
+						<div class='date'>
+							<?php the_date(); ?>
+						</div>
+						<div class='pic'><?php the_post_thumbnail(); ?></div>
+						<div class='clear'></div>
+					</div>
+					<div class='title-text'>
+						<div class='title'><a href='$url'><h2><?php the_title(); ?></h2></a></div>
+						<div class='text'><p><?php the_content(); ?></p></div>
+					</div>	
+				</div>";
+	}
+
+
+	return $html;
+}
