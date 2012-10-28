f7d37a3c0350a10732be5c58d182e1165fa366a2
mkdesign <me@mkdesign.ir>
2012-10-28 14:23:31 +0330

    news has been chabge completely

---
+++ Finall-Project/single-news.php
@@ -0,0 +1,29 @@
+<?php get_template_part('inc/header'); ?>
+	<section class="contain-news">
+		<div class="middle">
+			<div class="title" id='tit'></div>
+			
+				<?php
+				$news = new WP_Query(array('post_type' => 'news'));
+					while ($news->have_posts()) {
+						$news->the_post();
+				?>
+				<div class="news">
+					<div class="date-pic">
+						<div class="date">
+							<?php the_date(); ?>
+						</div>
+						<div class="pic"><?php the_post_thumbnail(); ?></div>
+						<div class="clear"></div>
+					</div>
+					<div class="title-text">
+						<div class="title"><h2><?php the_title(); ?></h2></div>
+						<div class="text"><p><?php the_content(); ?></p></div>
+					</div>	
+				</div>
+				<?php
+					}
+				?>
+		</div>
+	</section>
+<?php get_template_part('inc/footer'); ?>
