05bec74e5d08c3eadd93bf6eb25b02028a36d424
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2012-10-29 21:22:26 +0330

    add slide in index page with wp

--- Finall-Project/inc/content.php
+++ Finall-Project/inc/content.php
@@ -1,23 +1,39 @@
 	<section class="contain">
 		<div class="middle">
-			<div class="title" id='tit'></div>
-			<div class="banner">
-				<?php
-					$images_path = bloginfo('template_url')."/images/slide";
-					$images      = scandir($images_path);
-					
-					foreach ($images as $img) {
-						$img_exp = explode('.' , $img);
-						$img_exp_end = end($img_exp);
-						$img_type = strtolower($img_exp_end);
-						
-						if($img_type == 'jpg' || $img_type == 'png'){
+			<?php
+				if (have_posts()){
+					the_post();
+			?>
+				<div id='tit'><?php the_title(); ?></div>
+				<div class="banner">
+					<?php
+						$slide_attach= array(
+							'numberposts' => '-1',
+							'orderby '=> 'menu_order',  
+							'order'=> 'ASC',  
+							'post_mime_type' => 'image', 
+							'post_parent' => $post->ID, 
+							'post_status' => null, 
+							'post_type' => 'attachment'
+						);
 
-							echo "<div style=\"background-image : url('$images_path/$img');\"></div>";
+						$images= get_children($slide_attach);
+
+						if ($images){
+
+							foreach ($images as $img) {
+								
+									$img_larg= wp_get_attachment_image_src($img->ID,'large');
+									echo "<div style='background-image: url(\"$img_larg[0]\")'></div>";
+							}
 						}
-					}
-				?>         
-			</div>
+					?>     
+				</div>
+
+			<?php
+				}
+			?>
+
 			<?php get_template_part('inc/widget-recent'); ?>
 		</div>
 	</section>
--- Finall-Project/index.php
+++ Finall-Project/index.php
@@ -1,4 +1,8 @@
 <?php
+	/*
+		Template Name: home page
+	*/
+		
 	get_template_part('./inc/header');
 
 	get_template_part('./inc/content');
